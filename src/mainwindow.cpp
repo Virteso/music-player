94ed458771647ca223bd4de2674713945902720e
Rasmus <rasmus.lille0@gmail.com>
2025-05-25 11:12:58 +0300

    GUI test build

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,9 +6,42 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // Initialize VLC instance
+    const char *vlc_args[] = {
+        "--quiet", "--no-xlib"
+    };
+    vlcInstance = libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
+    mediaPlayer = libvlc_media_player_new(vlcInstance);
+
+    // Connect buttons
+    connect(ui->buttonPlay, &QPushButton::clicked, this, &MainWindow::playMedia);
+    connect(ui->buttonPause, &QPushButton::clicked, this, &MainWindow::pauseMedia);
 }
 
 MainWindow::~MainWindow()
 {
+    if (mediaPlayer)
+        libvlc_media_player_release(mediaPlayer);
+    if (vlcInstance)
+        libvlc_release(vlcInstance);
     delete ui;
+}
+
+void MainWindow::playMedia()
+{
+    QString path = ui->lineEditPath->text();
+    if (path.isEmpty())
+        return;
+
+    if (media)
+        libvlc_media_release(media);
+
+    media = libvlc_media_new_path(path.toStdString().c_str());
+    libvlc_media_player_set_media(mediaPlayer, media);
+    libvlc_media_player_play(mediaPlayer);
+}
+
+void MainWindow::pauseMedia()
+{
+    libvlc_media_player_pause(mediaPlayer);
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,22 +2,28 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <vlc/vlc.h>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
+private:
+    Ui::MainWindow *ui;
+
+    libvlc_instance_t *vlcInstance = nullptr;
+    libvlc_media_player_t *mediaPlayer = nullptr;
+    libvlc_media_t *media = nullptr;
+
+private slots:
+    void playMedia();
+    void pauseMedia();
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
